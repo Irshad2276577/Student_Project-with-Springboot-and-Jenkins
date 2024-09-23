350bb76d24ec3cb0025d99f5a7cba9fee80ef5f8
Gurunath Jadhav <gurunath.jadhav.842@gmail.com>
2024-09-23 11:14:37 +0530

    one endpoint is added

---
+++ src/main/java/com/chatApplication/controller/ChatController.java
@@ -0,0 +1,17 @@
+package com.chatApplication.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/chat")
+public class ChatController {
+
+    @GetMapping
+    public List getALlChats(){
+        return List.of("Hii","Hello");
+    }
+}
